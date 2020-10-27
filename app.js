43796ea227258f04d54ff09999562b74509087d1
TudorPetruToma <63647566+TudorPetruToma@users.noreply.github.com>
2020-10-27 13:37:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 let budgetController = (function(){
 
+        let x = 10;
+
+        let add = function(a){
+            return x + a;
+        }
+
+        return{
+            publicTest: function(b){
+                return add(b)
+            }
+        }
 })();
+
+let UIController = (function(){
+    // Some code
+})()
+
+let controller = (function(budgetCtrl, UICtrl){
+
+    let z = budgetCtrl.publicTest(50)
+
+    return{
+        anotherPublic: function(){
+            console.log(z)
+        }
+    }
+
+})(budgetController, UIController)
