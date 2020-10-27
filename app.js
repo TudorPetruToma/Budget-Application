4b9a1bd24b73ed95dc24f4252193a4425d60222f
LechMael <63647566+LechMael@users.noreply.github.com>
2020-10-27 16:33:17 +0200

    Ev Listeners Setup
    Defined main event listeners - keypress and click and added a hero image for the app

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+// BUDGET CONTROLLER
 let budgetController = (function(){
 
         //some code
 })();
 
+
+//UI Controller
 let UIController = (function(){
     // Some code
 })()
 
+
+// Global App Controller
 let controller = (function(budgetCtrl, UICtrl){
 
-    //some code
+    let ctrlAddItem = function(){
+        // 1. get the field input data
+        // 2. add the item to the budget controller
+        // 3. add the item to the UI
+        // 4. Calculta the budget
+        // 5. Display the budget
+        
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+       if(event.code === 'Enter' || event.which === 13){
+        ctrlAddItem();
+       }
+    })
 
 })(budgetController, UIController)
